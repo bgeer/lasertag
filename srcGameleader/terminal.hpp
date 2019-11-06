585dcd99f1132c275f762910d71c7a98a5445822
Peter Schenkels <43245077+Peter-Schenkels@users.noreply.github.com>
2019-11-06 23:43:14 +0100

    add doxygen

--- srcGameleader/terminal.hpp
+++ srcGameleader/terminal.hpp
@@ -3,6 +3,13 @@
 
 #include "hwlib.hpp"
 
+
+/// \@file
+
+/// \brief
+/// Terminal used to write text to a hwlib window
+/// \details
+/// Terminal used to write text to a hwlib window in 2 different character sizes
 class terminal {
 private:
 
@@ -25,9 +32,25 @@ public:
         display.flush();
     }
 
+    /// \brief 
+    /// clear display buffer
+    /// \details
+    /// This function is used to clear the buffer of a HWLIB window.
     void clear();
+    /// \brief
+    /// Flushes display buffer
+    /// \details
+    /// This function is used for flushing the display buffer of a HWLIB window
     void flush();
+    /// \brief 
+    /// Writes 8x8 characters in to the display buffer
+    /// \details
+    /// Adds newline and writes 8x8 of the hwlib window display buffer
     void write8x8( const char text[] );
+    /// \brief 
+    /// Writes 16x16 characters in to the display buffer
+    /// \details
+    /// Adds newline and writes 16x16 of the hwlib window display buffer
     void write16x16( const char text[] );    
 };
 
