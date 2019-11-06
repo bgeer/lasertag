74e032215f713647e939e127f025433496a30b56
Peter Schenkels <43245077+Peter-Schenkels@users.noreply.github.com>
2019-11-07 00:31:11 +0100

    add doxygen

--- srcRunGame/oledController.hpp
+++ srcRunGame/oledController.hpp
@@ -25,19 +25,35 @@ public:
     drawStartMessage();
     oled.flush();
     }
-
+    /// \brief
+    /// Flushes display buffer
+    /// \details
+    /// This function is used for flushing the display buffer of a HWLIB window
     void flush();
-
+    /// \brief 
+    /// clear display buffer
+    /// \details
+    /// This function is used to clear the buffer of a HWLIB window.
     void clear();
 
+    /// \brief
+    /// Draws player number to the display buffer.
     void drawPlayerNumber(int value);
 
+    /// \brief
+    /// Draws player hp and remaining time to display buffer.
     void drawHpTime(int hp, int remainTimeSec);
 
+    /// \brief
+    /// Draws Gameover screen to the display buffer
     void drawGameOver();
 
+    /// \brief
+    /// Draws countdown to display buffer
     void drawGameCountdown(int sec);
 
+    /// \brief
+    /// Draws the start message to the display buffer
     void drawStartMessage();
 };
 
