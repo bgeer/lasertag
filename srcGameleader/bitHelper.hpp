042c3b1952eda183f885edea70e432a90c346fdf
Peter Schenkels <43245077+Peter-Schenkels@users.noreply.github.com>
2019-11-06 23:33:12 +0100

    Update bitHelper.hpp
    add doxygen

--- srcGameleader/bitHelper.hpp
+++ srcGameleader/bitHelper.hpp
@@ -2,21 +2,31 @@
 #define BIT_HELPER_HPP
 
 #include "hwlib.hpp"
+// \@file
 
 
-//van rechts naar links (little Endian)
+/// \brief
+/// Gets bit from givin index in givin uint16_t
+/// \details
+/// Gets bit from givin index in givin uint16_t in little endian.
 bool getbit(uint8_t index, const uint16_t & byte){
     return (byte & (1 << (index)));
 }
 
-//set bit to a bool
+/// \brief
+/// Sets bit in givin index in givin uint16_t to givin bool
+/// \details
+/// Sets bit in givin index in givin uint16_t to givin bool in little Endian.
 void setbit(uint8_t index, uint16_t & byte, bool set){
    
     byte ^= (-set ^ byte) & (1UL << index);
     
 }
 
-//prints in big endian
+/// \brief
+/// Prints givin uint16_t
+/// \details
+/// Prints givin uint16_t in Big endian
 void printBit(const uint16_t & byte){
     for(int i = 16; i > 0; i--){
         hwlib::cout << getbit(i, byte) ;
@@ -24,14 +34,10 @@ void printBit(const uint16_t & byte){
     hwlib::cout << '\n';
 }
 
-//Encode to ir protocol
-// uint16_t encode(uint16_t message){
-//     for(int i = 14; i >= 9; i--){
-//         setbit(6 - i, message, (getbit(15 - i, message) ^ getbit(10 - i, message)));
-//     }
-//     return message;
-// }
-
+/// \brief
+/// Encodes bit for use in IR protocol.
+/// \details
+/// Encodes bit for use in IR protocol.
 uint16_t encode(uint16_t message) {
     for (int i = 4; i >= 0; i--) {
         setbit(i, message, (getbit(i + 10, message) ^ getbit(i + 5, message)));
@@ -39,7 +45,10 @@ uint16_t encode(uint16_t message) {
     return message;
 }
 
-//Check if bit came as a whole
+/// \brief
+/// Decodes bit for use in IR protocol and returns true if byte is received without losingdata.
+/// \details
+/// Decodes bit for use in IR protocol and returns true if byte is received without losingdata.
 bool check(const uint16_t & message){
     for(int i = 4; i >= 0; i--){
         if (getbit(i, message) != bool(getbit(i+10, message) ^ getbit(i+5, message))){
