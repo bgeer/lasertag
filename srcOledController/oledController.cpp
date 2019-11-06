4b82eab38f19a1475c04da290733b057080b6e2e
bgeer <bas.vandergeer@student.hu.nl>
2019-11-06 10:48:52 +0100

    small fix oled & updated doxyfile

--- srcOledController/oledController.cpp
+++ srcOledController/oledController.cpp
@@ -11,6 +11,7 @@ void oledController::clear(){
 
 void oledController::drawPlayerNumber(int value){
     playerPart
+        << "\f"
         << "----------------\n"
         << "Player ID = " << value << "\n"
         << "----------------";
@@ -20,9 +21,10 @@ void oledController::drawHpTime(int hp, int remainTimeSec){
     int sec = remainTimeSec % 60;
     int min = remainTimeSec / 60;
     scoreTimePart
+        << "\f" << "\n"
         << "HP = " << hp << "\n"
         << "Time: " << min << ":" << sec << "\n"
-        << "----------------\n";
+        << "\n----------------\n";
 }
 
 void oledController::drawGameOver(){
